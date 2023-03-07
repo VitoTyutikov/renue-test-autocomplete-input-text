6aeb0936b7b97921d12f7c5abfcabf30109fd9e9
vitaly <dbnf11dbnf@gmail.com>
2023-03-07 18:13:40 +0300

    start think for don't read file every time from start

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # renue-test-autocomplete-input-text
+
+### Run project
+1. ```mvn clean package```
+2. ```java -jar -Xmx7m target/renue-test-autocomplete-input-text-1.0-SNAPSHOT.jar 1 ```
--- src/main/java/vitaly/Main.java
+++ src/main/java/vitaly/Main.java
@@ -1,7 +1,32 @@
 package vitaly;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        System.out.print("Enter text : ");
+        FileReader fileReader = new FileReader("src/main/resources/airports.csv");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        Scanner LookScanner = new Scanner(System.in);
+        String lineFromFile;
+        String lookingText = LookScanner.nextLine();
+        while(!lookingText.equals("!quit")){
+            lineFromFile = bufferedReader.readLine();
+
+            while(lineFromFile!=null){
+                System.out.println(lineFromFile);
+
+                lineFromFile = bufferedReader.readLine();
+            }
+            bufferedReader.reset();
+            lookingText = LookScanner.nextLine();
+        }
+        bufferedReader.close();
+        fileReader.close();
+
     }
 }
