f4dd8e0eab07627722346660da4dbfab9d84229c
vitaly <dbnf11dbnf@gmail.com>
2023-03-10 00:34:05 +0300

    search works fast

--- src/main/java/vitaly/Main.java
+++ src/main/java/vitaly/Main.java
@@ -1,118 +1,85 @@
-/*
+
 package vitaly;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.print("Enter text : ");
-        FileReader fileReader = new FileReader("src/main/resources/airports.csv");
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        Scanner LookScanner = new Scanner(System.in);
-        String lineFromFile;
-        String lookingText = LookScanner.nextLine();
-        while(!lookingText.equals("!quit")){
-            lineFromFile = bufferedReader.readLine();
-
-            while(lineFromFile!=null){
-                System.out.println(lineFromFile);
-
-                lineFromFile = bufferedReader.readLine();
-            }
-            bufferedReader.reset();
-            lookingText = LookScanner.nextLine();
-        }
-        bufferedReader.close();
-        fileReader.close();
-
-    }
-}*/
-package vitaly;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class AirportSearch {
+public class Main {
     private static final String FILE_NAME = "src/main/resources/airports.csv";
-    private static final int LINE_SIZE = 100;
-    private static final int MAX_MEMORY_USAGE = 7 * 1024 * 1024;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         if (args.length != 1) {
-            System.err.println("Usage: java AirportSearch <column_number>");
+            System.out.println("Usage: java -jar -Xmx7m target/renue-test-autocomplete-input-text-1.0-SNAPSHOT.jar  <column_number>");
             System.exit(1);
         }
-
         int columnNumber = Integer.parseInt(args[0]);
-        if (columnNumber <= 0) {
-            System.err.println("Column number should be positive");
+        if (columnNumber < 1) {
+            System.out.println("Column number should >=1");
             System.exit(1);
         }
 
+        System.out.print("Enter text : ");
         try (RandomAccessFile file = new RandomAccessFile(new File(FILE_NAME), "r")) {
-            long fileSize = file.length();
-            long memoryUsagePerLine = LINE_SIZE + 8; // overhead for storing line start position
-            long maxLinesToRead = Math.min(fileSize / memoryUsagePerLine, MAX_MEMORY_USAGE / memoryUsagePerLine);
+            HashMap<Character, ArrayList<Long>> pointers = new HashMap<>();
+            String line;
+            long position = file.getFilePointer();
+            while ((line = file.readLine()) != null) {
+                String[] fields = line.replace("\"", "").split(",");
 
-            // read file line by line and store starting position of each line
-            List<Long> lineStartPositions = new ArrayList<>();
-            long lineStartPosition = 0;
-            while (lineStartPositions.size() < maxLinesToRead && lineStartPosition < fileSize) {
-                lineStartPositions.add(lineStartPosition);
-                file.seek(lineStartPosition);
-                file.readLine();
-                lineStartPosition = file.getFilePointer();
+                if (pointers.containsKey(fields[columnNumber - 1].charAt(0))) {
+                    ArrayList<Long> positions = pointers.get(fields[columnNumber - 1].charAt(0));
+
+                    positions.add(position);
+                    pointers.replace(fields[columnNumber - 1].charAt(0), positions);
+                } else {
+
+                    ArrayList<Long> positions = new ArrayList<>();
+                    positions.add(position);
+                    pointers.put(fields[columnNumber - 1].charAt(0), positions);
+                }
+                position = file.getFilePointer();
             }
+            Scanner lookScanner = new Scanner(System.in);
+            String searchText;
 
-            // search for matching lines
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter search text: ");
-            String searchText = scanner.nextLine().trim();
-            while (!searchText.equals("!quit")) {
-                long startTime = System.currentTimeMillis();
+
+            while (!(searchText = lookScanner.nextLine()).equals("!quit")) {
                 if (searchText.isEmpty()) {
                     break;
                 }
-                List<LineData> matchingLines = new ArrayList<>();
-                for (long startPosition : lineStartPositions) {
-                    String line = readLineFromFile(file, startPosition);
-                    String[] fields = line.split(",") ;
+                long startTime = System.currentTimeMillis();
+
+                ArrayList<Long> positions = pointers.get(searchText.charAt(0));
+                ArrayList<LineData> matchingLines = new ArrayList<>();
+                if (positions == null) {
+                    System.out.print("Symbols not found. Try again or quit using <!quit>. Enter text: ");
+                    continue;
+                }
+                for (Long pos : positions) {
+                    file.seek(pos);
+                    String line1 = file.readLine();
+                    String[] fields = line1.replace("\"", "").split(",");
                     if (fields[columnNumber - 1].startsWith(searchText)) {
-                        matchingLines.add(new LineData(fields[columnNumber - 1], line));
+                        matchingLines.add(new LineData(fields[columnNumber - 1], line1));
                     }
                 }
-                long endTime = System.currentTimeMillis();
                 Collections.sort(matchingLines);
-
+                long endTime = System.currentTimeMillis();
                 for (LineData lineData : matchingLines) {
                     System.out.println(lineData.toString());
                 }
-                System.out.println("Count of found lines = "+matchingLines.size()+" time = "+(endTime-startTime));
+                System.out.println("Count of found lines = " + matchingLines.size() + " time = " + (endTime - startTime));
                 System.out.print("Enter search text: ");
-                searchText = scanner.nextLine().trim();
             }
+            file.close();
         } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
+            System.out.println("Invalid column number");
             System.exit(1);
         }
-    }
-
-    private static String readLineFromFile(RandomAccessFile file, long startPosition) throws IOException {
-        file.seek(startPosition);
-        StringBuilder builder = new StringBuilder();
-        int c;
-        while ((c = file.read()) != -1 && c != '\n') {
-            builder.append((char) c);
-        }
-        return builder.toString();
     }
 
     private static class LineData implements Comparable<LineData> {
@@ -135,3 +102,4 @@ public class AirportSearch {
         }
     }
 }
+
